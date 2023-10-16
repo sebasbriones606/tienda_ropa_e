59c9df429bc3f5717d70f5a4320ba281d78d49bc
sebasbriones606 <143129288+sebasbriones606@users.noreply.github.com>
2023-10-15 19:31:10 -0600

    Update urls.py
    Ufa ufa la patunfla

--- tienda_website_project/homepage/urls.py
+++ tienda_website_project/homepage/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [ 
     path("",views.home)
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
