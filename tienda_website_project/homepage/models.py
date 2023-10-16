bad5eeeee5c0fca30680ceeddb040c3b2f73316e
sebasbriones606 <143129288+sebasbriones606@users.noreply.github.com>
2023-10-15 19:28:04 -0600

    Update models.py
    Mi parte Sebas Briones

--- tienda_website_project/homepage/models.py
+++ tienda_website_project/homepage/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.shortcuts import render
+from .models import Product
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/')
+    stock_quantity = models.PositiveIntegerField()
+    is_available = models.BooleanField(default=True)
+    # Otros campos relevantes
+    
